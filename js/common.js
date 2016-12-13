60732c9a6a7957b25198cd860531a0d5ec2f1548
haitao <869040858@qq.com>
2016-12-13 21:45:09 +0800

    修改链接前

--- js/common.js
+++ js/common.js
@@ -7,3 +7,56 @@ $(function () {
         $(this).hide().val('');
     })
 });
+
+function resizeLb() {
+    //重新算高度 赋值给swiper
+    var height=$('.img').eq(0).height();
+    $('.swiper-container').css('height',height);
+
+    //进度点
+    $('.pagination').css('top',height-30);
+    console.log(height);
+}
+//        $(function () {
+//            resizeLb();
+//
+//        });
+window.onload = function () {
+    resizeLb();
+};
+window.onresize=function () {
+    resizeLb();
+};
+
+
+if(parseFloat(document.body.clientWidth)< 930){
+    //导航效果
+    var btnShow=false;
+    $('.navBtn').on('click',function (e) {
+        if(btnShow){
+            $('.navList').removeClass('active');
+            btnShow=false;
+            e.stopPropagation()
+        }else {
+            $('.navList').addClass('active');
+//                    $('.navList').css('overflow','auto');
+            btnShow=true;
+            e.stopPropagation();
+        }
+    });
+
+    $('ul .navList li').on('click',function (e) {
+        e.stopPropagation()
+    });
+
+//            $('body').on('click',function () {
+//                $('.navList').removeClass('active');
+//                btnShow=false;
+//            })
+
+
+    window.onload = function () {
+        resizeLb();
+        point();
+    };
+}
